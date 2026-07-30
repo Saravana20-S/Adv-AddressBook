9f24e14119337871eeac78f7e90d1ad574f5d2a0
Saravana20-S <iamsaravanan1347@gmail.com>
2026-07-30 10:53:37 +0530

    UC2: Table Created

--- main/java/com/bridgelabz/advaddressbook/Main.java
+++ main/java/com/bridgelabz/advaddressbook/Main.java
@@ -2,6 +2,11 @@ package com.bridgelabz.advaddressbook;
 
 public class Main {
     public static void main(String[] args) {
+
+        //UC1
         System.out.println("SQL DataBase Created");
+
+        //UC2
+        System.out.println("Address_Book Table Created");
     }
 }
