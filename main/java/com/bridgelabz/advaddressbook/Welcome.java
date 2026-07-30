bdeb649234415302fa5f8acf147de050bb94c1cc
Saravana20-S <iamsaravanan1347@gmail.com>
2026-07-30 10:30:55 +0530

    Project Setup

---
+++ main/java/com/bridgelabz/advaddressbook/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.advaddressbook;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Advance Address Book Project Setup");
+    }
+}
